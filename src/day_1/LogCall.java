7090cf7551d340c53b3b254959c763cb7d44b7d0
Jovid <62119364+jovidimohammad@users.noreply.github.com>
2020-05-08 15:36:08 -0400

    Update LogCall.java

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -3,5 +3,6 @@ package day_1;
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("LogCall class was created");
+        System.out.println("Changes");
     }
 }
